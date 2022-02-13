21299ae5fe6a97b447a895e5817d803c3245b46d
Aleksandr <iwolframium@bk.ru>
2022-02-14 01:11:22 +0300

    engine's ready

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let hideWindow = () => {
-    for (let i = 0; i < document.getElementsByClassName('modalwindow').length; i++) {
+    for (let i = 0; i < document.getElementsByClassName('modalwindow').length; i++)
         document.getElementsByClassName('modalwindow')[i].classList.add('hidden')
-    }
 }
 
 let showAddWindow = () => {
@@ -12,14 +11,21 @@ let showEditWindow = () => {
     document.querySelector('.editwindow').classList.remove('hidden');
 }
 
+let showPreview = () => {
+    document.querySelector('.addwindow-preview').setAttribute('src', document.querySelector('.addwindow-cover').value);
+}
+
+let initPreviewBtn = () => {
+    document.querySelector('.previewBtn').addEventListener('click', showPreview)
+}
+
 let initAddBtn = () => {
     document.querySelector('.showBtn').addEventListener('click', showAddWindow)
 }
 
 let initCancelBtn = () => {
-    for (let i = 0; i < document.getElementsByClassName('cancelBtn').length; i++) {
+    for (let i = 0; i < document.getElementsByClassName('cancelBtn').length; i++)
         document.getElementsByClassName('cancelBtn')[i].addEventListener('click', hideWindow)
-    }
 }
 
 let initDeleteBtn = () => {
@@ -31,20 +37,20 @@ let initDeleteBtn = () => {
 }
 
 let initEditBtn = () => {
-    let editBtn = document.getElementsByClassName('shelf-book-opt-edit');
-    for (let i = 0; i < editBtn.length; i++) {
-        editBtn[i].addEventListener('click', (event) => {
+    for (let i = 0; i < document.getElementsByClassName('shelf-book-opt-edit').length; i++) {
+        document.getElementsByClassName('shelf-book-opt-edit')[i].addEventListener('click', (event) => {
             showEditWindow()
+            let currentCover = event.target.parentNode.parentNode.children[0].firstChild.firstChild;
             let currentTitle = event.target.parentNode.parentNode.children[1].children[0];
             let currentAuthor = event.target.parentNode.parentNode.children[1].children[1];
             let currentYear = event.target.parentNode.parentNode.children[1].children[2];
-            document.querySelector('.editwindow-title').value = event.target.parentNode.parentNode.children[1].children[0].innerText;
-            document.querySelector('.editwindow-author').value = event.target.parentNode.parentNode.children[1].children[1].innerText;
-            document.querySelector('.editwindow-year').value = event.target.parentNode.parentNode.children[1].children[2].innerText;
-            
-
+            document.querySelector('.editwindow-cover').value = currentCover.getAttribute('src');
+            document.querySelector('.editwindow-title').value = currentTitle.innerText;
+            document.querySelector('.editwindow-author').value = currentAuthor.innerText;
+            document.querySelector('.editwindow-year').value = currentYear.innerText;
             let saveBtn = document.querySelector('.saveBtn')
             saveBtn.addEventListener('click', () => {
+                currentCover.setAttribute('src', document.querySelector('.editwindow-cover').value);
                 currentTitle.innerText = document.querySelector('.editwindow-title').value;
                 currentAuthor.innerText = document.querySelector('.editwindow-author').value;
                 currentYear.innerText = document.querySelector('.editwindow-year').value;
@@ -57,6 +63,30 @@ let initEditBtn = () => {
     }
 }
 
+let addBook = () => {
+    if (document.querySelector('.addwindow-title').value && document.querySelector('.addwindow-author').value && document.querySelector('.addwindow-year').value) {
+        document.querySelector('.shelf').insertAdjacentHTML('beforeend', `<tr class="shelf-book">
+        <td class="shelf-book-image"><div class="shelf-book-image-cover"><img src="${document.querySelector('.addwindow-cover').value}"></div></td>
+        <td class="shelf-book-desc">
+            <h1>${document.querySelector('.addwindow-title').value}</h1>
+            <h2>${document.querySelector('.addwindow-author').value}</h2>
+            <h2>${document.querySelector('.addwindow-year').value}</h2>
+        </td>
+        <td class="shelf-book-opt">
+            <button class="shelf-book-opt-edit">Редактировать</button>
+            <button class="shelf-book-opt-delete">Удалить</button>
+        </td>
+    </tr>`)
+        hideWindow()
+        initDeleteBtn()
+        initEditBtn()
+        document.querySelector('.addwindow-cover').value = '';
+        document.querySelector('.addwindow-title').value = '';
+        document.querySelector('.addwindow-author').value = '';
+        document.querySelector('.addwindow-year').value = '';
+    }
+}
+
 initAddBtn()
 
 initCancelBtn()
@@ -65,28 +95,4 @@ initDeleteBtn()
 
 initEditBtn()
 
-let addBook = () => {
-    let title = document.querySelector('.addwindow-title').value;
-    let author = document.querySelector('.addwindow-author').value;
-    let year = document.querySelector('.addwindow-year').value;
-    if (title && author && year) {
-        document.querySelector('.shelf').insertAdjacentHTML('beforeend', `<tr class="shelf-book">
-        <td class="shelf-book-image"></td>
-        <td class="shelf-book-desc">
-            <h1>${title}</h1>
-            <h2>${author}</h2>
-            <h2>${year}</h2>
-        </td>
-        <td class="shelf-book-opt">
-            <button class="shelf-book-opt-edit">Редактировать</button>
-            <button class="shelf-book-opt-delete">Удалить</button>
-        </td>
-    </tr>`)
-
-        hideWindow()
-
-        initDeleteBtn()
-
-        initEditBtn()
-    }
-}
+initPreviewBtn()
